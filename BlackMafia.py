94c7ac63ee4952f4076962afe72d6aff3df87cf9
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-06-07 15:41:00 +0500

    Update BlackMafia.py

--- BlackMafia.py
+++ BlackMafia.py
@@ -18,7 +18,9 @@ count = 0
 check = 0
 result = 0
 def masuk():
-        print('\n\n\t\t≺ \033[1;36mBlackMafia ● FACEBOOK LOGIN ● lovehacker\033[0m ≻\n\n')
+        print('\n\n\t\t≺ \033[1;36m●   Black Mafia  ● \033[0m ≻\n\n')
+        print('\n\n\t\t≺ \033[1;36m●   03094161457  ● \033[0m ≻\n\n')
+        print('\n\n\t\t≺ \033[1;36m● FACEBOOK LOGIN ● \033[0m ≻\n\n')
         try:
                 cek = open("cookies").read()
         except FileNotFoundError:
